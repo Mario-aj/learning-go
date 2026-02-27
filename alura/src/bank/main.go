912dbe8503c6291330bbeb412dbca81256a8d446
mario-aj <mariojorge1997@mail.ru>
2026-02-27 11:42:17 +0100

    feat: Add deposit method to Account struct and demonstrate its usage in main.

--- alura/src/bank/main.go
+++ alura/src/bank/main.go
@@ -9,19 +9,6 @@ type Account struct {
 	balance       float64
 }
 
-func (account *Account) withdraw(value float64) string {
-	if value <= 0 {
-		return "You are trying to withdraw a negative value"
-	}
-
-	if value > account.balance {
-		return "You do not have enough balance"
-	}
-
-	account.balance -= value
-	return "Withdraw successful"
-}
-
 func main() {
 	account := Account{
 		"Mario Jorge",
@@ -35,4 +22,32 @@ func main() {
 	withdrawMessage := account.withdraw(100)
 	fmt.Println(withdrawMessage)
 	fmt.Println("Your new balance is: ", account.balance)
+
+	fmt.Println("__________________________________")
+
+	messaage, value := account.deposit(1250)
+	fmt.Println(messaage)
+	fmt.Printf("Your new balance is: %.2f \n", value)
+}
+
+func (account *Account) withdraw(value float64) string {
+	if value <= 0 {
+		return "You are trying to withdraw a negative value"
+	}
+
+	if value > account.balance {
+		return "You do not have enough balance"
+	}
+
+	account.balance -= value
+	return "Withdraw successful"
+}
+
+func (account *Account) deposit(value float64) (string, float64) {
+	if value <= 0 {
+		return "You are trying to deposit a negative value", account.balance
+	}
+
+	account.balance += value
+	return "Deposit successful", account.balance
 }
