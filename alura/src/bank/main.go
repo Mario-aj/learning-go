53a016ea65c4eabac04f2d0c3b0b2aec46669c22
mario-aj <mariojorge1997@mail.ru>
2026-02-27 11:25:26 +0100

    feat: Implement account withdrawal functionality for `Account` struct including validation.

--- alura/src/bank/main.go
+++ alura/src/bank/main.go
@@ -9,6 +9,19 @@ type Account struct {
 	balance       float64
 }
 
+func (account *Account) withdraw(value float64) string {
+	if value <= 0 {
+		return "You are trying to withdraw a negative value"
+	}
+
+	if value > account.balance {
+		return "You do not have enough balance"
+	}
+
+	account.balance -= value
+	return "Withdraw successful"
+}
+
 func main() {
 	account := Account{
 		"Mario Jorge",
@@ -17,5 +30,9 @@ func main() {
 		559.99,
 	}
 
-	fmt.Println(account)
+	fmt.Println(account.balance)
+
+	withdrawMessage := account.withdraw(100)
+	fmt.Println(withdrawMessage)
+	fmt.Println("Your new balance is: ", account.balance)
 }
