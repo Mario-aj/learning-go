d5433fe40519a88122cac967924c3880936d982e
mario-aj <mariojorge1997@mail.ru>
2026-03-01 09:24:29 +0100

    feat: Enhance web application with dynamic and static content serving, including 404 error handling and new HTML files.

--- building-web-applications/module1/go.mod
+++ building-web-applications/module1/go.mod
@@ -2,4 +2,9 @@ module module1
 
 go 1.25.0
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.11.2 // indirect
+)
--- building-web-applications/module1/main.go
+++ building-web-applications/module1/main.go
@@ -3,20 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-func testeHandler(w http.ResponseWriter, r *http.Request) {
+const (
+	Port = ":3000"
+)
 
+func serveDynamic(w http.ResponseWriter, _ *http.Request) {
+	response := "The time now is " + time.Now().String()
+
+	fmt.Fprintln(w, response)
+}
+
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static.html")
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	pageId := vars["id"]
+	filename := "files/" + pageId + ".html"
+
+	_, err := os.Stat(filename)
+
+	if err != nil {
+		filename = "files/404.html"
+	}
+
+	http.ServeFile(w, r, filename)
 }
 
 func main() {
-	router := mux.NewRouter()
 
-	router.HandleFunc("/teste", testeHandler)
+	router := mux.NewRouter()
+	router.HandleFunc("/", serveDynamic)
+	router.HandleFunc("/static", serveStatic)
+	router.HandleFunc("/pages/{id}", pageHandler)
 
 	http.Handle("/", router)
-
-	fmt.Println("Everything is going to be alright")
+	http.ListenAndServe(Port, nil)
 }
