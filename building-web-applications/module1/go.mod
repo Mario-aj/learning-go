e9c41b856875285875582af572a33cf889aab509
mario-aj <mariojorge1997@mail.ru>
2026-03-01 22:22:51 +0100

    serving page dynamically by connecting to DB and list by id

--- building-web-applications/module1/go.mod
+++ building-web-applications/module1/go.mod
@@ -3,8 +3,8 @@ module module1
 go 1.25.0
 
 require (
-	filippo.io/edwards25519 v1.2.0 // indirect
-	github.com/go-sql-driver/mysql v1.9.3 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.11.2 // indirect
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/gorilla/mux v1.8.1
 )
+
+require filippo.io/edwards25519 v1.2.0 // indirect
--- building-web-applications/module1/main.go
+++ building-web-applications/module1/main.go
@@ -1,50 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
-	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 const (
-	Port = ":3000"
+	DBHost     = "127.0.0.1"
+	DBPort     = "3306"
+	DBUser     = "root"
+	DBPassword = "rootpassword"
+	DBName     = "go_blog_post_database"
+	Port       = ":3000"
 )
 
-func serveDynamic(w http.ResponseWriter, _ *http.Request) {
-	response := "The time now is " + time.Now().String()
+var database *sql.DB
 
-	fmt.Fprintln(w, response)
+type Page struct {
+	Title   string
+	Content string
+	Date    string
 }
 
-func serveStatic(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static.html")
-}
-
-func pageHandler(w http.ResponseWriter, r *http.Request) {
+func ServePage(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-
 	pageId := vars["id"]
-	filename := "files/" + pageId + ".html"
+	page := Page{}
 
-	_, err := os.Stat(filename)
+	fmt.Println(pageId)
+
+	err := database.QueryRow("SELECT page_title,page_content,page_date FROM pages WHERE id=?", pageId).Scan(&page.Title, &page.Content, &page.Date)
 
 	if err != nil {
-		filename = "files/404.html"
+		log.Println("Couldn't get page: ", pageId)
+		log.Println(err.Error())
 	}
 
-	http.ServeFile(w, r, filename)
+	html := `<html><head><title>` + page.Title +
+		`</title></head><body><h1>` + page.Title + `</h1><div>` +
+		page.Content + `</div></body></html>`
+
+	fmt.Fprintln(w, html)
 }
 
 func main() {
+	dbConn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DBUser, DBPassword, DBHost, DBPort, DBName)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("Couldn't connect to" + DBName)
+		log.Println(err.Error())
+	}
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", serveDynamic)
-	router.HandleFunc("/static", serveStatic)
-	router.HandleFunc("/pages/{id}", pageHandler)
+	database = db
 
-	http.Handle("/", router)
-	http.ListenAndServe(Port, nil)
+	routes := mux.NewRouter()
+	routes.HandleFunc("/page/{id:[0-9]+}", ServePage)
+	http.Handle("/", routes)
+
+	log.Println("Server is running on port", Port)
+
+	err = http.ListenAndServe(Port, nil)
+
+	if err != nil {
+		log.Panic("Couldn't start server", err.Error())
+	}
 }
