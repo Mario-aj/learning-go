f2c6a3b6a91a260bd927a28ef751100f38afda2c
mario-aj <mariojorge1997@mail.ru>
2026-05-30 12:47:10 +0100

    extract magic numbers into constants

--- learn-go-with-tests/hello-world/interaction/interation.go
+++ learn-go-with-tests/hello-world/interaction/interation.go
@@ -1,9 +1,12 @@
 package interaction
 
+
+const repeatQuantity = 5;
+
 func Repeat(char string) string {
 	var repeat string 
 
-	for i:= 0; i < 5; i++ {
+	for i:= 0; i < repeatQuantity; i++ {
 		repeat = repeat + char
 	}
 
