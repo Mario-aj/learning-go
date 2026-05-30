c3b680707b705fd8d288eb21049fa1d740c86d96
mario-aj <mariojorge1997@mail.ru>
2026-05-30 12:57:00 +0100

    adding benchmark

--- learn-go-with-tests/hello-world/interaction/interation_test.go
+++ learn-go-with-tests/hello-world/interaction/interation_test.go
@@ -2,11 +2,17 @@ package interaction
 
 import "testing"
 
-func TestInteration(t *testing.T){
+func TestInteraction(t *testing.T){
 	repeat := Repeat("a")
 	expected := "aaaaa"
 
 	if repeat != expected {
 		t.Errorf("expected '%s' but got '%s'", expected, repeat)
+	}
+}
+
+func BenchmarkInteraction(b *testing.B) {
+	for i:=0; i < b.N; i++ {
+		Repeat("a")
 	}
 }
