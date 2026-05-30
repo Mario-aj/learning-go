224290918f8b84786fd2a5394c0901453468ee08
mario-aj <mariojorge1997@mail.ru>
2026-05-30 11:20:55 +0100

    add language greet requirement

--- learn-go-with-tests/hello-world/hello.go
+++ learn-go-with-tests/hello-world/hello.go
@@ -3,16 +3,28 @@ package main
 import "fmt"
 
 const prefixHello = "Hello, "
+const spanish = "Spanish"
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, " 
+const french = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 
 	if name == "" {
 		name = "world!"
+	}
+
+	if language == spanish {
+		return spanishPrefix + name
+	}
+
+	if language == french {
+		return frenchPrefix + name	
 	}
 
 	return prefixHello + name
 }
 
 func main() {
-	fmt.Println(Hello("world!"))
+	fmt.Println(Hello("world!", ""))
 }
--- learn-go-with-tests/hello-world/hello_test.go
+++ learn-go-with-tests/hello-world/hello_test.go
@@ -14,7 +14,7 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Say hello to people", func(t*testing.T){
-		result := Hello("Chris")
+		result := Hello("Chris", "")
 		expected := "Hello, Chris"
 
 
@@ -22,9 +22,23 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("Say 'Hello, world!' when a empty string will be passed", func(t *testing.T) {
-		result:=Hello((""))
+		result:=Hello("", "")
 		expected := "Hello, world!"
 
+		verifyCorrectMessage(t, result, expected)
+	})
+
+	t.Run("In spanish", func(t *testing.T) {
+		result := Hello("Mario", "Spanish")
+		expected := "Hola, Mario"
+
+		verifyCorrectMessage(t, result, expected)
+	})
+
+	t.Run("Should greet in french", func(t *testing.T) {
+		result := Hello("Mario", "French")
+		expected := "Bonjour, Mario"
+		
 		verifyCorrectMessage(t, result, expected)
 	})
 }
