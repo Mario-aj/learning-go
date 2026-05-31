696269690f0c6b7588ce95ff84fcbcf519a768d2
mario-aj <mariojorge1997@mail.ru>
2026-05-31 13:35:46 +0100

    first step with arrays and slices

---
+++ learn-go-with-tests/array_and_slice/sum_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("should sum the collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		result := Sum(numbers)
+		expected := 15
+
+		if expected != result {
+			t.Errorf("Result '%d', expected '%d', data %v", result, expected, numbers)
+		}
+	})
+
+	t.Run("should sum collect of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		result := Sum(numbers)
+		expected := 6
+
+		if expected != result {
+			t.Errorf("Result '%d', expected '%d', data %v", result, expected, numbers)
+		}
+	})
+}
