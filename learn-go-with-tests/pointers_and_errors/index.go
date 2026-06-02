e7373cf98ba63fc5992670eb3fa1941103ab4743
mario-aj <mariojorge1997@mail.ru>
2026-06-02 11:11:02 +0100

    creating personalized types and implement the withdraw method

--- learn-go-with-tests/pointers_and_errors/index.go
+++ learn-go-with-tests/pointers_and_errors/index.go
@@ -1,13 +1,25 @@
 package main
 
+import "fmt"
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(quantity int) {
+func (w *Wallet) Deposit(quantity Bitcoin) {
 	w.balance += quantity
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Withdraw(quantity Bitcoin) {
+	w.balance -= quantity
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- learn-go-with-tests/pointers_and_errors/index_test.go
+++ learn-go-with-tests/pointers_and_errors/index_test.go
@@ -3,14 +3,29 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
-	result := wallet.Balance()
-	expected := 10
+		result := wallet.Balance()
+		expected := Bitcoin(10)
 
-	if result != expected {
-		t.Errorf("result %d, expected %d", result, expected)
-	}
+		if result != expected {
+			t.Errorf("result %s, expected %s", result, expected)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		result := wallet.Balance()
+		expected := Bitcoin(10)
+
+		if result != expected {
+			t.Errorf("result %s, expected %s", result, expected)
+		}
+	})
 }
