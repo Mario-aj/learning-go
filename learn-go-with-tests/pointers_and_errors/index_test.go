c9a975a275db588b0742f99ae8f4374818d68356
mario-aj <mariojorge1997@mail.ru>
2026-06-02 10:43:57 +0100

    passing receiver by ref

---
+++ learn-go-with-tests/pointers_and_errors/index_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	result := wallet.Balance()
+	expected := 10
+
+	if result != expected {
+		t.Errorf("result %d, expected %d", result, expected)
+	}
+}
