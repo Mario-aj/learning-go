e5238a2aac759419845840d19ba7ea8c151c6068
mario-aj <mariojorge1997@mail.ru>
2026-06-05 09:56:13 +0100

    small error handle update

--- learn-go-with-tests/dep_injection/index.go
+++ learn-go-with-tests/dep_injection/index.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func Greeting(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func HandleMyGreeting(w http.ResponseWriter, r *http.Request) {
+	Greeting(w, "World!")
+}
+
 func main() {
-	Greeting(os.Stdout, "Mario")
+	err := http.ListenAndServe(":5000", http.HandlerFunc(HandleMyGreeting))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
