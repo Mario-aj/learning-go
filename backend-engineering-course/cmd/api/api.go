2222ee2ff6048d2c0ff34592880a8dd805816387
mario-aj <mariojorge1997@mail.ru>
2026-06-10 08:20:45 +0100

    config DB connection pool

--- backend-engineering-course/cmd/api/api.go
+++ backend-engineering-course/cmd/api/api.go
@@ -10,8 +10,16 @@ import (
 	"github.com/mario-aj/social/internal/store"
 )
 
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
 type config struct {
 	address string
+	db      dbConfig
 }
 
 type application struct {
--- backend-engineering-course/cmd/api/main.go
+++ backend-engineering-course/cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/mario-aj/social/internal/db"
 	"github.com/mario-aj/social/internal/env"
 	"github.com/mario-aj/social/internal/store"
 )
@@ -11,9 +12,21 @@ import (
 func main() {
 	cfg := config{
 		address: env.GetString("ADDR", ":8081"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
